5db29cf1e9238d39eb6315e4cf26bf1d8e55a8f6
Giles Knap <giles.knap@diamond.ac.uk>
2022-06-17 14:29:06 +0000

    comment the pmac handler

--- src/builder2ibek/convertors/pmac.py
+++ src/builder2ibek/convertors/pmac.py
@@ -1,12 +1,21 @@
+"""
+The convertor handler module for pmac support module
+"""
+
 from builder2ibek.types import Entity, Generic_IOC
 
+# The prefix for Builder XML Tags that this support module uses
 xml_component = "pmac"
 
+# The ibek schema for the Generic IOC that compiles this support module
+# (currently not used)
 schema = (
     "https://github.com/epics-containers/ioc-pmac/releases/"
     "download/1.2.1/ioc.ibek.schema.yaml"
 )
 
+# A list of Tags and their default attributes
+# These should match defaults supplied in builder.py __init__()
 defaults = {
     "pmac.GeoBrick": {
         "numAxes": 8,
@@ -17,18 +26,22 @@ defaults = {
 
 
 def handler(entity: Entity, entity_type: str, ioc: Generic_IOC):
+    """
+    XML to YAML specialist convertor function for the pmac support module
+    """
     if entity_type == "pmacDisableLimitsCheck":
         entity.remove("name")
 
     elif entity_type == "dls_pmac_asyn_motor":
-        # TODO unnecessary difference - could just call this dls_pmac_asyn_motor?
+        # TODO unnecessary difference -
+        # could just call this dls_pmac_asyn_motor in ibek definition ?
         entity.type = "pmac.DlsPmacAsynMotor"
 
         entity.rename("PORT", "Controller")
         entity.remove("SPORT")
 
     elif entity_type == "dls_pmac_cs_asyn_motor":
-        # TODO unnecessary difference - could just call this DlsPmacAsynMotor?
+        # TODO unnecessary difference
         entity.type = "pmac.DlsPmacCsAsynMotor"
 
         entity.rename("PORT", "Controller")
