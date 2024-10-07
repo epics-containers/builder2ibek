3938a2ae78c2bf4ee00e53361cef84a4eb9b4b57
Max Herbert <max.herbert@diamond.ac.uk>
2024-10-07 11:22:24 +0100

    Updated test format

--- tests/test_file_conversion.py
+++ tests/test_file_conversion.py
@@ -1,15 +1,22 @@
-import subprocess
 import sys
-import os
+import subprocess
+
 
 def test_cli_version():
-    conversion_samples = ["tests/samples/BL45P-MO-IOC-01.xml",
-                           "tests/samples/BL99P-EA-IOC-05.xml"]    
-    
+    conversion_samples = [
+        "tests/samples/BL45P-MO-IOC-01.xml",
+        "tests/samples/BL99P-EA-IOC-05.xml",
+    ]
+
     for sample in conversion_samples:
-        cmd = [sys.executable, "-m", "builder2ibek", "file", "--yaml", "out.yaml", sample]
+        cmd = [
+            sys.executable,
+            "-m",
+            "builder2ibek",
+            "file",
+            "--yaml",
+            "out.yaml",
+            sample,
+        ]
         result = subprocess.run(cmd)
         assert result.returncode == 0
-
-    
-
