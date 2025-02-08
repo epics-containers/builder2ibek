5a85efec4273d7d824c48bc2b7b5bbd4ab1c6bd1
Giles Knap <giles.knap@diamond.ac.uk>
2025-02-08 09:05:42 +0000

    improve comparison comment handling

--- src/builder2ibek/dbcompare.py
+++ src/builder2ibek/dbcompare.py
@@ -4,10 +4,14 @@ from pathlib import Path
 
 # extract record type, name and fields from an EPICS database file
 regex_record = re.compile(
-    r"(#)* *record *\( *([^,]*), *\"?([^\"]*)\"? *\)[\s\S]*?{([\s\S]*?)}"
+    r"^(?<!#) *record *\( *([^,]*), *\"?([^\"]*)\"? *\)[\s\S]*?{([\s\S]*?)}",
+    re.MULTILINE,
 )
 # extract field name and value from the fields section of the above
-regex_field = re.compile(r'(#)* *field *\( *([^,]*), *"?([^"]*)"? *\)')
+regex_field = re.compile(
+    r'^(?<!#) *field *\( *([^,]*), *"?([^"]*)"? *\)',
+    re.MULTILINE,
+)
 
 
 @dataclass
@@ -21,19 +25,29 @@ class EpicsDb:
 def normalize_float_records(s: set[str]) -> set[str]:
     normalised = set()
     for item in s:
+        item = item.replace(" MS", "")
+        item = item.replace(" PP", "")
         try:
             parts = item.split(maxsplit=1)
             if len(parts) == 1:
                 # blank fields are the same as absent fields
                 continue
             name, value = parts
-            # default values can be ignored
+            # default values can also be ignored
             if name == "SCAN" and value == "Passive":
                 continue
             normalised.add(f"{name} {float(value)}")
         except (ValueError, IndexError):
             normalised.add(item)
     return normalised
+
+
+def check_ignore_list(ignore: list[str], string: str) -> bool:
+    for s in ignore:
+        if re.findall(s, string):
+            # print(f"ignoring record: {string}")
+            return True
+    return False
 
 
 def compare_dbs(
@@ -51,49 +65,39 @@ def compare_dbs(
     for db in [old, new]:
         db.text = db.path.read_text()
         for record in regex_record.finditer(db.text):
-            if record.group(1) is not None:
-                # skip commented out records
-                continue
-            rec_str = f"{record.group(2)} {record.group(3)}"
+            rec_str = f"{record.group(1)} {record.group(2)}"
             # throw away duplicates in the old db
-            if rec_str not in db.fields or db == new:
-                db.records.add(rec_str)
-                db.fields[rec_str] = set()
-                for rec_field in regex_field.finditer(record.group(4)):
-                    if rec_field.group(1) is not None:
-                        # skip commented out fields
-                        continue
-                    db.fields[rec_str].add(f"{rec_field.group(2)} {rec_field.group(3)}")
+            if rec_str in db.fields and db == old:
+                continue
+            # throw away records that match the ignore list
+            if check_ignore_list(ignore, rec_str):
+                continue
+            db.records.add(rec_str)
+            db.fields[rec_str] = set()
+            for rec_field in regex_field.finditer(record.group(3)):
+                field = f"{rec_field.group(1)} {rec_field.group(2)}"
+                if check_ignore_list(ignore, field):
+                    continue
+                db.fields[rec_str].add(field)
 
     old_only = sorted(old.records - new.records)
     new_only = sorted(new.records - old.records)
     both = sorted(old.records & new.records)
 
-    old_only_filtered = old_only.copy()
-    new_only_filtered = new_only.copy()
-    for filtered, unfiltered in [
-        (old_only_filtered, old_only),
-        (new_only_filtered, new_only),
-    ]:
-        for record_str in unfiltered:
-            for s in ignore:
-                if s in record_str:
-                    filtered.remove(record_str)
-
     result = (
         "*******************************************************************\n"
         + "Records in original but not in new:\n\n"
-        + "\n".join(old_only_filtered)
+        + "\n".join(old_only)
         + "\n\n"
         + "*******************************************************************\n"
         + "Records in new but not in original:\n\n"
-        + "\n".join(new_only_filtered)
+        + "\n".join(new_only)
         + "\n\n"
         + "*******************************************************************\n"
         + f"records in original:    {len(old.records)}\n"
         f"  records in new:         {len(new.records)}\n"
-        f"  records missing in new: {len(old_only_filtered)}\n"
-        f"  records extra in new:   {len(new_only_filtered)}\n"
+        f"  records missing in new: {len(old_only)}\n"
+        f"  records extra in new:   {len(new_only)}\n"
         + "*******************************************************************\n"
     )
 
@@ -103,6 +107,7 @@ def compare_dbs(
         new_norm = normalize_float_records(new.fields[record_str])
         if old_norm != new_norm:
             result += f"\nfields for '{record_str}' are different between the two DBs"
+            # result += f"\ndiff: {old_norm.difference(new_norm)}"
 
     if not output:
         print(result)
