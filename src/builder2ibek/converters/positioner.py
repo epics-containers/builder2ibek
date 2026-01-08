82b532e0aece3e3533b47dfb773e0b28c1f4beb2
Huy Duong <huy.duong@diamond.ac.uk>
2026-01-08 16:07:52 +0000

    Moved changes from pmac.py to positioner.py, more changes

--- src/builder2ibek/converters/positioner.py
+++ src/builder2ibek/converters/positioner.py
@@ -14,3 +14,21 @@ GDA_PLUGINS = Path(__file__).parent / "gdaPlugins.yaml"
 def handler(entity: Entity, entity_type: str, ioc: Generic_IOC):
     if entity_type == "positioner":
         entity.DEADBAND = str(entity.DEADBAND)
+
+    elif entity_type == "motorpositioner":
+        # find the motor: dls_pmac_asyn_motor name = original motorpositioner.motor
+        # get the motor's PV and EGU
+        # the motor's PV is claculated by concatenating its P and M params
+        # set motorpositioner.motor = motor's PV
+        # set motorpositioner.EGU = motor's PV
+        motors = [
+            e
+            for e in ioc.raw_entities
+            if e["type"].endswith(("dls_pmac_asyn_motor", "dls_pmac_cs_asyn_motor"))
+            and e["name"] == entity.motor
+        ]
+        assert len(motors) == 1
+        motor = motors[0]
+        motor_pv = motor["P"] + motor["M"]
+        entity.motor = motor_pv
+        entity.EGU = motor["EGU"]
