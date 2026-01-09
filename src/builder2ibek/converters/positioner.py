0214767fbee376efc8aa03e5042d9e55fb774933
Huy Duong <huy.duong@diamond.ac.uk>
2026-01-09 08:11:52 +0000

    Got update to motorpositioner's converter working

--- src/builder2ibek/converters/positioner.py
+++ src/builder2ibek/converters/positioner.py
@@ -16,7 +16,8 @@ def handler(entity: Entity, entity_type: str, ioc: Generic_IOC):
         entity.DEADBAND = str(entity.DEADBAND)
 
     elif entity_type == "motorpositioner":
-        # find the motor: dls_pmac_asyn_motor name = original motorpositioner.motor
+        # find the motor whose name = original motorpositioner.motor
+        # the motor is of type "dls_pmac_asyn_motor" or "dls_pmac_cs_asyn_motor"
         # get the motor's PV and EGU
         # the motor's PV is claculated by concatenating its P and M params
         # set motorpositioner.motor = motor's PV
@@ -24,8 +25,10 @@ def handler(entity: Entity, entity_type: str, ioc: Generic_IOC):
         motors = [
             e
             for e in ioc.raw_entities
-            if e["type"].endswith(("dls_pmac_asyn_motor", "dls_pmac_cs_asyn_motor"))
-            and e["name"] == entity.motor
+            if e.get("type", "").endswith(
+                ("dls_pmac_asyn_motor", "dls_pmac_cs_asyn_motor")
+            )
+            and e.get("name") == entity.motor
         ]
         assert len(motors) == 1
         motor = motors[0]
