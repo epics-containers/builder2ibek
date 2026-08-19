8fa1d4a6db1f0763f1f14e0be3237a7f452ffa37
Ollie Copping <oliver.copping@diamond.ac.uk>
2026-08-19 08:23:30 +0000

    Update OXCryo to convert to oxCryo, and added more CS types

--- src/builder2ibek/converters/OXCryo.py
+++ src/builder2ibek/converters/OXCryo.py
@@ -7,15 +7,24 @@ xml_component = "OXCryo"
 @globalHandler
 def handler(entity: Entity, entity_type: str, ioc: Generic_IOC):
     """
-    Convert OXCryo entities to the newer OXCS700asyn support module.
+    Convert OXCryo entities to the newer oxCryo support module.
     """
 
     if entity_type == "OXCS700":
-        entity.type = "OXCS700asyn.OXCS700asyn"
+        entity.type = "oxCryo.OXCS700"
         entity.remove("name")
-        entity.remove("DISABLE_COMMS")
-        if not hasattr(entity, "Q") or not entity.Q:
-            entity.Q = ""
+        # entity.remove("DISABLE_COMMS")
+        # if not hasattr(entity, "Q") or not entity.Q:
+        #     entity.Q = ""
 
     elif entity_type == "OXNH700":
+        entity.type = "oxCryo.OXNH700"
+        entity.remove("name")
+
+    elif entity_type == "OXCB700":
+        entity.type = "oxCryo.OXCB700"
+        entity.remove("name")
+
+    elif entity_type == "OXCB800":
+        entity.type = "oxCryo.OXCB800"
         entity.remove("name")
