dd9bb650e5c09ca8f3601b190bd39e7101536278
OCopping <OCopping@users.noreply.github.com>
2026-08-19 12:18:59 +0000

    deploy: 3484b1b02efebb0b73ef5730cccd1afa6cd97c30

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.3.0b2.dev14+g62cc0a5d6',
+    VERSION: '2.3.0b2.dev18+g3484b1b02',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
